2bf4acf655c078e4d170dcb4ae3992bb19438731
kpdecker <kpdecker@gmail.com>
2015-01-12 17:39:37 -0600

    Add test for published files

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var Circus = require('circus'),
     CircusStylus = require('../lib'),
     Glob = require('glob'),
+    Path = require('path'),
     webpack = require('webpack');
 
 var expect = require('chai').expect,
@@ -53,7 +54,7 @@ describe('loader integration', function() {
       },
 
       stylus: {
-        includesDir: __dirname + '/fixtures/styles/includes/**/*.styl',
+        includesDir: __dirname + '/fixtures/styles/includes/**/*',
         defines: {
           $isAndroid: false
         }
@@ -69,12 +70,13 @@ describe('loader integration', function() {
       expect(compilation.errors).to.be.empty;
       expect(compilation.warnings).to.be.empty;
 
-      expect(Object.keys(compilation.assets)).to.eql([
-        'bundle.js',
+      expect(Object.keys(compilation.assets).sort()).to.eql([
         '0.bundle.css',
+        'bundle.js',
+        'bundle.js.map',
         'circus.json',
         'test/fixtures/styles/includes/red.styl',
-        'bundle.js.map'
+        'test/fixtures/styles/includes/url.gif'
       ]);
 
       // Verify the actual css content
@@ -99,10 +101,10 @@ describe('loader integration', function() {
       },
 
       stylus: {
-        includesDir: __dirname + '/fixtures/styles/includes/**/*.styl',
+        includesDir: __dirname + '/fixtures/styles/includes/**/*',
         remap: function(file) {
-          expect(file).to.equal('test/fixtures/styles/includes/red.styl');
-          return 'styles/includes/red.styl';
+          expect(file).to.match(/test\/fixtures\/styles\/includes\//);
+          return 'styles/includes/' + Path.basename(file);
         },
         defines: {
           $isAndroid: false
@@ -119,12 +121,13 @@ describe('loader integration', function() {
       expect(compilation.errors).to.be.empty;
       expect(compilation.warnings).to.be.empty;
 
-      expect(Object.keys(compilation.assets)).to.eql([
-        'bundle.js',
+      expect(Object.keys(compilation.assets).sort()).to.eql([
         '0.bundle.css',
+        'bundle.js',
+        'bundle.js.map',
         'circus.json',
         'styles/includes/red.styl',
-        'bundle.js.map'
+        'styles/includes/url.gif',
       ]);
 
       var input = Fs.readFileSync(__dirname + '/fixtures/styles/includes/red.styl').toString(),
@@ -175,8 +178,6 @@ describe('loader integration', function() {
     var vendorEntry = path.resolve(__dirname + '/fixtures/stylus.js'),
         entry = path.resolve(__dirname + '/fixtures/require.js');
 
-    outputDir = 'tmp/';
-
     var config = {
       context: __dirname,
       entry: vendorEntry,
@@ -188,7 +189,7 @@ describe('loader integration', function() {
 
       },
       stylus: {
-        includesDir: __dirname + '/fixtures/styles/includes/**/*.styl',
+        includesDir: __dirname + '/fixtures/styles/includes/**/*',
         remap: function(file) {
           return file.replace(/fixtures\//, '');
         }
@@ -229,6 +230,73 @@ describe('loader integration', function() {
 
         var output = Fs.readFileSync(outputDir + '/0.bundle.css').toString();
         expect(output).to.match(/\.red \{/);
+        expect(output).to.match(/url\("d41d8cd98f00b204e9800998ecf8427e.gif"\)/);
+
+        done();
+      });
+    });
+  });
+  it('should import published urls from components', function(done) {
+    var vendorEntry = path.resolve(__dirname + '/fixtures/stylus.js'),
+        entry = path.resolve(__dirname + '/fixtures/require.js');
+
+    var config = {
+      context: __dirname,
+      entry: vendorEntry,
+      output: {
+        component: 'vendor',
+
+        path: outputDir + '/vendor',
+        filename: 'vendor.js',
+
+      },
+      stylus: {
+        includesDir: __dirname + '/fixtures/styles/includes/**/*',
+        remap: function(file) {
+          return file.replace(/fixtures\//, '');
+        }
+      }
+    };
+    config = CircusStylus.config(config);
+    config = Circus.config(config);
+
+    webpack(config, function(err, status) {
+      expect(err).to.not.exist;
+
+      var compilation = status.compilation;
+      expect(compilation.errors).to.be.empty;
+      expect(compilation.warnings).to.be.empty;
+
+      var output = JSON.parse(Fs.readFileSync(outputDir + '/vendor/circus.json'));
+      output.published['d41d8cd98f00b204e9800998ecf8427e.gif'] = 'd41d8cd98f00b204e9800998ecf8427e.foo';
+      Fs.writeFileSync(outputDir + '/vendor/circus.json', JSON.stringify(output));
+
+      var config = {
+        entry: entry,
+
+        output: {
+          path: outputDir
+        },
+
+        resolve: {
+          modulesDirectories: [
+            outputDir
+          ]
+        }
+      };
+      config = CircusStylus.config(config);
+      config = Circus.config(config);
+
+      webpack(config, function(err, status) {
+        expect(err).to.not.exist;
+
+        var compilation = status.compilation;
+        expect(compilation.errors).to.be.empty;
+        expect(compilation.warnings).to.be.empty;
+
+        var output = Fs.readFileSync(outputDir + '/0.bundle.css').toString();
+        expect(output).to.match(/\.red \{/);
+        expect(output).to.match(/url\("d41d8cd98f00b204e9800998ecf8427e.foo"\)/);
 
         done();
       });
@@ -263,7 +331,7 @@ describe('loader integration', function() {
         },
 
         stylus: {
-          includesDir: __dirname + '/fixtures/styles/includes/**/*.styl'
+          includesDir: __dirname + '/fixtures/styles/includes/**/*'
         }
       };
       config = CircusStylus.config(config);
@@ -298,7 +366,7 @@ describe('loader integration', function() {
         },
 
         stylus: {
-          includesDir: __dirname + '/fixtures/styles/includes/**/*.styl'
+          includesDir: __dirname + '/fixtures/styles/includes/**/*'
         }
       };
       config = CircusStylus.config(config);
