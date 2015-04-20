2a829662d2fe32becd62d0182e8caaecb57c78d1
kpdecker <kpdecker@gmail.com>
2015-04-20 17:43:49 -0500

    Switch to eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,5 @@
-/*
-  gulpfile.js
-  ===========
-  Each task has been broken out into its own file in build/tasks. Any file in that folder gets
-  automatically required by the loop in ./gulp/index.js (required below).
-
-  To add a new task, simply add a new task file to ./build/tasks.
-*/
-
 var gulp = require('gulp'),
-
-    jscs = require('gulp-jscs'),
-    jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish'),
-
+    eslint = require('gulp-eslint'),
     istanbul = require('gulp-istanbul'),
     mocha = require('gulp-mocha');
 
@@ -23,15 +10,11 @@ var mochaTests = [
   '!test/fixtures/**/*.js'
 ];
 
-gulp.task('jshint', function() {
+gulp.task('eslint', function() {
   return gulp.src(source)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
-});
-
-gulp.task('jscs', function() {
-  return gulp.src(source)
-    .pipe(jscs());
+    .pipe(eslint())
+    .pipe(eslint.format('stylish'))
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('mocha', function() {
@@ -51,6 +34,6 @@ gulp.task('coverage', function(done) {
     });
 });
 
-gulp.task('lint', ['jshint', 'jscs']);
+gulp.task('lint', ['eslint']);
 gulp.task('test', ['coverage']);
 gulp.task('default', ['lint', 'test']);
